a0ea6a8d5e19459e64c62c9606881c7b92846a74
freankine212 <lilmichaeljr@gmail.com>
2023-12-01 21:59:47 -0700

    fixed mainmenu

--- prove/Develop05/MainMenu.cs
+++ prove/Develop05/MainMenu.cs
@@ -8,12 +8,14 @@ public class MainMenu
     private string _menu = $@"
     Welcome to your Goal menu
     ~~~~~~~~~~~~~~~~~~~~~~~~~
-    1. Simple Goal (quick and painless)
-    2. Eternal Goal (You're never gonna finish this, its just for your self)
-    3. Checklist Goal (Need something to keep you going throughout the challenge?)
-    4. Back to Start
+    1. New Goal Here
+    2. List Goals
+    3. Save Goals
+    4. Load Goals
+    5. Record Goal Event
+    6. Quit
     ~~~~~~~~~~~~~~~~~~~~~~~~~
-    What type of goal do you want to create?  ";
+    What do you wanna do: ";
 
         public string _userInput;
         private int _userChoice=0;
