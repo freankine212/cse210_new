9e098a5e3f26b2cb49b525372befdbf736fc7f4f
freankine212 <lilmichaeljr@gmail.com>
2023-12-09 11:32:47 -0700

    foundation4 done

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Exercise> exerciseList = new List<Exercise>();
+        
+        Running running = new Running("December 10, 2023", 60, 5);
+        exerciseList.Add(running);
+
+        StationaryBicycle stationaryBicycle = new StationaryBicycle("November 12, 2023", 60, 10);
+        exerciseList.Add(stationaryBicycle);
+
+        Swimming swimming = new Swimming("August 15, 2023", 30, 15);
+        exerciseList.Add(swimming);
+
+        Console.WriteLine("Exercises:");
+        Console.WriteLine();
+
+        foreach (Exercise exercise in exerciseList) 
+        {
+            Console.WriteLine(exercise.GetSummary());
+        }
     }
 }
